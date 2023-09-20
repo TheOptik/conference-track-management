c18c573cb675aa302fdce5dd42b8cc54404243d1
TheOptik <simon.huemmer@huemmer-satz.de>
2023-09-20 19:22:27 +0200

    initial application bootstrap

---
+++ src/main/kotlin/de/theoptik/conferencetrackmanagement/ConferenceTrackManagementApplication.kt
@@ -0,0 +1,11 @@
+package de.theoptik.conferencetrackmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConferenceTrackManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConferenceTrackManagementApplication>(*args)
+}
---
+++ src/test/kotlin/de/theoptik/conferencetrackmanagement/ConferenceTrackManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.theoptik.conferencetrackmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConferenceTrackManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
