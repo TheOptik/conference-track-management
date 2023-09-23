a46a452a18e672993d8139974cce2d28db987d99
TheOptik <simon.huemmer@huemmer-satz.de>
2023-09-23 18:35:28 +0200

    add more sessions to test data and partially implement track composer

--- src/main/kotlin/de/theoptik/conferencetrackmanagement/service/TrackComposer.kt
+++ src/main/kotlin/de/theoptik/conferencetrackmanagement/service/TrackComposer.kt
@@ -9,7 +9,34 @@ import org.springframework.stereotype.Service
 class TrackComposer {
 
     fun composeTracks(sessions: List<Session>): List<Track> {
-        TODO()
+        val morningSessions =
+            selectSessionsFromPool(listOf(), sessions, Track.MORNING_SESSIONS_MAXIMUM_LENGTH_IN_MINUTES)
+        val afternoonSessions =
+            selectSessionsFromPool(
+                listOf(),
+                sessions - morningSessions,
+                Track.AFTERNOON_SESSIONS_MAXIMUM_LENGTH_IN_MINUTES
+            )
+
+        return listOf(Track(morningSessions, afternoonSessions))
+    }
+
+
+    private fun selectSessionsFromPool(
+        selectedSessions: List<Session>, sessionPool: List<Session>, maxSessionsLengthInMinutes: Int
+    ): List<Session> {
+        if (selectedSessions.totalLengthInMinutes() == maxSessionsLengthInMinutes || sessionPool.isEmpty()) {
+            return selectedSessions
+        }
+
+        val poolHead = sessionPool.first()
+        val poolTail = sessionPool.subList(1, sessionPool.size)
+
+        if (selectedSessions.totalLengthInMinutes() + poolHead.lengthInMinutes > maxSessionsLengthInMinutes) {
+            return selectSessionsFromPool(selectedSessions, poolTail, maxSessionsLengthInMinutes)
+        }
+
+        return selectSessionsFromPool(selectedSessions + poolHead, poolTail, maxSessionsLengthInMinutes)
     }
 
 
--- src/test/kotlin/de/theoptik/conferencetrackmanagement/fixtures/SessionFixtures.kt
+++ src/test/kotlin/de/theoptik/conferencetrackmanagement/fixtures/SessionFixtures.kt
@@ -13,7 +13,18 @@ val ALL_SESSIONS = listOf(
     Session("Common Ruby Errors", 45),
     Session("Rails for Python Developers", 5),
     Session("Communicating Over Distance", 60),
-    Session("Accounting-Driven Development", 45)
+    Session("Accounting-Driven Development", 45),
+    Session("Woah", 30),
+    Session("Sit Down and Write", 30),
+    Session("Pair Programming vs Noise", 45),
+    Session("Rails Magic", 60),
+    Session("Ruby on Rails: Why We Should Move On", 60),
+    Session("Clojure Ate Scala (on my project)", 45),
+    Session("Programming in the Boondocks of Seattle", 30),
+    Session("Ruby vs. Clojure for Back-End Development", 30),
+    Session("Ruby on Rails Legacy App Maintenance", 60),
+    Session("A World Without HackerNews", 30),
+    Session("User Interface CSS in Rails Apps", 30),
 )
 
 val ALL_SESSIONS_UNPARSED = listOf(
@@ -24,7 +35,18 @@ val ALL_SESSIONS_UNPARSED = listOf(
     "Common Ruby Errors 45min",
     "Rails for Python Developers lightning",
     "Communicating Over Distance 60min",
-    "Accounting-Driven Development 45min"
+    "Accounting-Driven Development 45min",
+    "Woah 30min",
+    "Sit Down and Write 30min",
+    "Pair Programming vs Noise 45min",
+    "Rails Magic 60min",
+    "Ruby on Rails: Why We Should Move On 60min",
+    "Clojure Ate Scala (on my project) 45min",
+    "Programming in the Boondocks of Seattle 30min",
+    "Ruby vs. Clojure for Back-End Development 30min",
+    "Ruby on Rails Legacy App Maintenance 60min",
+    "A World Without HackerNews 30min",
+    "User Interface CSS in Rails Apps 30min"
 )
 
 object SessionFixtures {
