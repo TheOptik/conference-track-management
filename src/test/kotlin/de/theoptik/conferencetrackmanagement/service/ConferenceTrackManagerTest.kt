ad56755d33dc28d919e68d2b42420f4dd806f22e
TheOptik <simon.huemmer@huemmer-satz.de>
2023-09-24 16:06:21 +0200

    add first test for conference track manager

--- src/test/kotlin/de/theoptik/conferencetrackmanagement/service/ConferenceTrackManagerTest.kt
+++ src/test/kotlin/de/theoptik/conferencetrackmanagement/service/ConferenceTrackManagerTest.kt
@@ -1,6 +1,33 @@
 package de.theoptik.conferencetrackmanagement.service
 
+import de.theoptik.conferencetrackmanagement.fixtures.ALL_SESSIONS
+import de.theoptik.conferencetrackmanagement.fixtures.ALL_SESSIONS_UNPARSED
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.*
+
 internal class ConferenceTrackManagerTest {
 
+    @Test
+    fun allSessionNamesMustBeContainedInThePrintedConferenceProgram() {
+        val mockLineProvider = setupMockLineProvider(ALL_SESSIONS_UNPARSED)
+        val mockLineWriter: LineWriter = mock()
 
+        val conferenceTrackManager = ConferenceTrackManager(
+            SessionParser(), TrackComposer(), TimeTableComposer(), mockLineProvider, mockLineWriter
+        )
+
+        conferenceTrackManager.run()
+
+        ALL_SESSIONS.forEach { verify(mockLineWriter).println(contains(it.title)) }
+    }
+
+    fun setupMockLineProvider(lines: List<String>): LineProvider {
+        val firstLine = lines[0]
+        val followingLines = lines.subList(1, lines.size).toTypedArray()
+
+        val mockLineProvider: LineProvider = mock()
+        //ends with an empty line to signal the conference track manager that the input is over
+        `when`(mockLineProvider.nextLine()).thenReturn(firstLine, *followingLines, "")
+        return mockLineProvider
+    }
 }
