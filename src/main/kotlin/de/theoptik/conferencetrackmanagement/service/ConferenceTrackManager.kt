d7bf4d48bb142ce9edac97380fa785d7c3e1ba9a
TheOptik <simon.huemmer@huemmer-satz.de>
2023-09-24 18:06:35 +0200

    give user better feedback on wrong session format

--- src/main/kotlin/de/theoptik/conferencetrackmanagement/service/ConferenceTrackManager.kt
+++ src/main/kotlin/de/theoptik/conferencetrackmanagement/service/ConferenceTrackManager.kt
@@ -1,5 +1,6 @@
 package de.theoptik.conferencetrackmanagement.service
 
+import de.theoptik.conferencetrackmanagement.exception.SessionFormatException
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 
@@ -17,7 +18,18 @@ class ConferenceTrackManager(
         println()
 
         val sessions = generateSequence { lineProvider.nextLine() }.takeWhile { it.isNotBlank() }
-            .map { sessionParser.parseSession(it) }.toList()
+            .mapNotNull {
+                try {
+                    return@mapNotNull sessionParser.parseSession(it)
+                } catch (formatException: SessionFormatException) {
+                    println(formatException.message)
+                    println("Please provide the session in the following format: <session name> <session duration>")
+                    println("Session titles may not contain numbers, e.g: My Cool Session")
+                    println("Session duration may either be denoted in minutes or must be 'lightning', e.g: 45min.")
+                    println()
+                    return@mapNotNull null
+                }
+            }.toList()
 
         val tracks = trackComposer.composeTracks(sessions)
 
