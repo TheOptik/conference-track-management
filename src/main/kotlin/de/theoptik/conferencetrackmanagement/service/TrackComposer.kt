0c6792cc13e9a3d157ab377ba44351a672763f88
TheOptik <simon.huemmer@huemmer-satz.de>
2023-09-24 10:45:26 +0200

    refactor track composer implementation

--- src/main/kotlin/de/theoptik/conferencetrackmanagement/service/TrackComposer.kt
+++ src/main/kotlin/de/theoptik/conferencetrackmanagement/service/TrackComposer.kt
@@ -3,26 +3,29 @@ package de.theoptik.conferencetrackmanagement.service
 import de.theoptik.conferencetrackmanagement.extensions.totalLengthInMinutes
 import de.theoptik.conferencetrackmanagement.model.Session
 import de.theoptik.conferencetrackmanagement.model.Track
+import de.theoptik.conferencetrackmanagement.model.Track.Companion.AFTERNOON_SESSIONS_MAXIMUM_LENGTH_IN_MINUTES
+import de.theoptik.conferencetrackmanagement.model.Track.Companion.MORNING_SESSIONS_MAXIMUM_LENGTH_IN_MINUTES
 import org.springframework.stereotype.Service
 
 @Service
 class TrackComposer {
 
     fun composeTracks(sessions: List<Session>): List<Track> {
-        var pool = sessions
-        val tracks = mutableListOf<Track>()
+        return composeTracks(emptyList(), sessions)
+    }
 
-        while (pool.isNotEmpty()) {
-            val morningSessions =
-                selectSessionsFromPool(listOf(), pool, Track.MORNING_SESSIONS_MAXIMUM_LENGTH_IN_MINUTES)
-            val afternoonSessions = selectSessionsFromPool(
-                listOf(), pool - morningSessions, Track.AFTERNOON_SESSIONS_MAXIMUM_LENGTH_IN_MINUTES
-            )
-            pool = (pool - morningSessions) - afternoonSessions
-            tracks.add(Track(morningSessions, afternoonSessions))
+    private fun composeTracks(tracks: List<Track>, sessionPool: List<Session>): List<Track> {
+        if (sessionPool.isEmpty()) {
+            return tracks
         }
-        
-        return tracks
+        val morningSessions =
+            selectSessionsFromPool(emptyList(), sessionPool, MORNING_SESSIONS_MAXIMUM_LENGTH_IN_MINUTES)
+        val afternoonSessions = selectSessionsFromPool(
+            emptyList(), sessionPool - morningSessions, AFTERNOON_SESSIONS_MAXIMUM_LENGTH_IN_MINUTES
+        )
+        return composeTracks(
+            tracks + Track(morningSessions, afternoonSessions), (sessionPool - morningSessions) - afternoonSessions
+        )
     }
 
 
